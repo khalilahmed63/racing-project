aef0314154910e21d9bce2de0d3e4a2e569605ee
khalilahmed63 <panhwarkhalilahmed@gmail.com>
2023-07-10 01:05:11 +0500

    dropdown created in header

--- components/global/Header/Header.tsx
+++ components/global/Header/Header.tsx
@@ -1,5 +1,6 @@
-import { Group, Header, Image, createStyles, rem } from '@mantine/core';
+import { Autocomplete, Group, Header, Image, createStyles, rem } from '@mantine/core';
 import Link from 'next/link';
+import { IconSearch } from 'tabler-icons';
 import { ColorSchemeToggle } from '../Common/ColorSchemeToggle';
 
 const useStyles = createStyles((theme) => ({
@@ -47,35 +48,6 @@ export default function NavigationHeader() {
   const { classes } = useStyles();
   // const [isOpen, setIsOpen] = useState(false);
 
-  const links = [
-    {
-      label: 'HOME',
-      link: '/',
-    },
-    {
-      label: 'MOTOGP',
-      link: '/Motogp-race-result',
-    },
-    {
-      label: 'FORMULAONE',
-      link: 'https://www.google.com',
-    },
-    {
-      label: 'CONTACT',
-      link: 'https://www.wikipedia.org',
-    },
-  ];
-
-  const items = links.map((link) => (
-    <Link
-      key={link.label}
-      href={link.link}
-      className={classes.link}
-    >
-      {link.label}
-    </Link>
-  ));
-
   return (
     <>
       <div className="fixed top-0 z-[100] w-full ">
@@ -89,14 +61,41 @@ export default function NavigationHeader() {
 
             <Group>
               <Group ml={50} spacing={5} className={classes.links}>
-                {items}
+                <Link href="/" className={classes.link}>
+                  HOME
+                </Link>
+                <div className="group">
+                  <div className={`${classes.link} relative cursor-pointer`}>MOTOGP</div>
+                  <div className="hidden p-2 absolute bg-white rounded-md group-hover:block">
+                    <Link href="/Motogp-race-result" className={`${classes.link} py-3`}>
+                      RACE RESULT
+                    </Link>
+                    <Link href="/Motogp-race-result" className={`${classes.link} py-3`}>
+                      STANDING
+                    </Link>
+                  </div>
+                </div>
+                <div className="group">
+                  <div className={`${classes.link} relative cursor-pointer`}>FORMULAONE</div>
+                  <div className="hidden p-2 absolute bg-white rounded-md group-hover:block">
+                    <Link href="#" className={`${classes.link} py-3`}>
+                      COMMING SOON
+                    </Link>
+                    <Link href="#" className={`${classes.link} py-3`}>
+                      COMMING SOON
+                    </Link>
+                  </div>
+                </div>
+                <Link href="/" className={classes.link}>
+                  CONTACT
+                </Link>
               </Group>
-              {/* <Autocomplete
+              <Autocomplete
                 className={classes.search}
                 placeholder="Search"
                 icon={<IconSearch size="1rem" stroke={1.5} />}
                 data={['React', 'Angular', 'Vue', 'Next.js', 'Riot.js', 'Svelte', 'Blitz.js']}
-              /> */}
+              />
             </Group>
             <Group>
               <div className="mb-6">
